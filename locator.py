9f7d0d3a5f7e9f9a69ceb6459814a9451934db22
ckim115 <christina.hayoung@gmail.com>
2024-04-05 14:48:23 -0700

    Implement hotel search at a constant address

--- locator.py
+++ locator.py
@@ -1,4 +1,6 @@
 import googlemaps
+import pandas as pd
+import time
 # atm very basic, just asks for an input for starting location and end location
 # Read API key from txt
 API = open("Google_Maps_API_Key.txt", "r")
@@ -6,11 +8,25 @@ APIKey = API.read()
 
 maps = googlemaps.Client(key = APIKey)
 
-#finding cur location and end location
-start = input("Where are you currently?\n")
-end = input("Where will you end?\n")
+def miles_to_meters(miles:float):
+    return miles*1_609.344
 
-#calculating distance
-distance = maps.directions(start, end) #gives instructions to reach destination
-#to only get distance and time:
-print(distance[0]['legs'][0]['distance']['text'], "and", distance[0]['legs'][0]['duration']['text'])
+#finding cur location and end location
+loc = (37.3507608,-122.0264249) # TODO: figure out how to find current location
+max_distance = miles_to_meters(15)
+
+response = maps.places_nearby(
+    location=loc,
+    keyword='hotel',
+    radius=max_distance,
+)
+print(response.keys())
+hotel_list = response.get('results') #where we will store all our found hotels
+
+df = pd.DataFrame(hotel_list)
+df.to_csv('list_of_nearby_hotels.csv', index=False)
+
+# #calculating distance
+# distance = maps.directions(start, end) #gives instructions to reach destination
+# #to only get distance and time:
+# print(distance[0]['legs'][0]['distance']['text'], "and", distance[0]['legs'][0]['duration']['text'])

b8f82cc6b7f03e44b840e0fe8567bf283ffb3e85
Jared SJ <jared.soliven@sjsu.edu>
2024-04-10 08:53:07 -0700

    moved read csv from hotels to main

--- Hotels.py
+++ Hotels.py
@@ -38,21 +38,21 @@ class Hotels():
 #hotel1 = Hotels("Hotel De Anza", "Cupertino", "Great hotel of De Anza", 120, "www.DeAnzaHotel.com", 4.1)
 #hotel1.printHotelInfo()
 
-hotelList = list()
-with open('list_of_nearby_hotels.csv', mode='r') as file:
-    csvFile = csv.reader(file)
-    i = 0
-    for lines in csvFile:
-        if lines[5] == "name": # ignore csv file format / basically checking if name of hotel is name
-            continue
-        else:
-            tempHotel = "hotel"+str(i)
-            tempHotel = Hotels(lines[5], lines[15], "Great hotel", 408+i, "www."+lines[5]+".com", lines[10])
-            hotelList.append(tempHotel)
-            i += 1
+# hotelList = list()
+# with open('list_of_nearby_hotels.csv', mode='r') as file:
+#     csvFile = csv.reader(file)
+#     i = 0
+#     for lines in csvFile:
+#         if lines[5] == "name": # ignore csv file format / basically checking if name of hotel is name
+#             continue
+#         else:
+#             tempHotel = "hotel"+str(i)
+#             tempHotel = Hotels(lines[5], lines[15], "Great hotel", 408+i, "www."+lines[5]+".com", lines[10])
+#             hotelList.append(tempHotel)
+#             i += 1
 
-#print(hotelList) prints all hotel objects
+# #print(hotelList) prints all hotel objects
 
-for hotel in hotelList:
-    hotel.printHotelInfo()
-    print() # spacer in between hotel prints
+# for hotel in hotelList:
+#     hotel.printHotelInfo()
+#     print() # spacer in between hotel prints
--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template, request # import flask class methods
-from Users import Users # import User class
-from Hotels import Hotels # import Hotel class
+from Users import * # import User class
+from Hotels import * # import Hotel class
 
 app = Flask(__name__)
 
 # put regular functions definitions before routing functions
+def readCSV():
+    hotelList = list()
+    with open('list_of_nearby_hotels.csv', mode='r') as file:
+        csvFile = csv.reader(file)
+        i = 0
+        for lines in csvFile:
+            if lines[5] == "name": # ignore csv file format / basically checking if name of hotel is name
+                continue
+            else:
+                tempHotel = "hotel"+str(i)
+                tempHotel = Hotels(lines[5], lines[15], "Great hotel", 408+i, "www."+lines[5]+".com", lines[10])
+                hotelList.append(tempHotel)
+                i += 1
+    #print(hotelList) prints all hotel objects
+    for hotel in hotelList:
+        hotel.printHotelInfo()
+        print() # spacer in between hotel prints
 
 # functions used to direct webpages
 @app.route('/')
@@ -15,6 +32,13 @@ def hello_world():
 @app.route('/index', methods=['POST', 'GET'])
 def index_page():
     return render_template('index.html', form=request.form)
+
+# take in user selected location and send in updated index info with hotels in area
+# display hotels in area
+# call read csv func to get updated csv file with hotels in area
+# readCSV()
+# send data to page
+# def updated_index_page():
 
 @app.route('/HotelInfo', methods=['POST', 'GET'])
 def hotel_info_page():
